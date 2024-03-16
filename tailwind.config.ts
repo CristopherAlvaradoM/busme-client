018ba15ba359e01fe0ee2273b254286837c288c4
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-03-16 17:03:51 -0600

    add corporation colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,37 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      transparent: "transparent",
+      black: "#000",
+      white: "#fff",
+      primary: {
+        50: "#D6FFFA",
+        100: "#A3FFF4",
+        200: "#00FADD",
+        300: "#00E0C6",
+        400: "#00C7B0",
+        500: "#00A895",
+        600: "#009987",
+        700: "#008575",
+        800: "#007063",
+        900: "#004D44",
+        950: "#003832"
+      },
+      muted: {
+        50: "#FAFAFA",
+        100: "#F5F5F5",
+        200: "#EDEDED",
+        300: "#E0E0E0",
+        400: "#D6D6D6",
+        500: "#CACACA",
+        600: "#B5B5B5",
+        700: "#A1A1A1",
+        800: "#878787",
+        900: "#616161",
+        950: "#454545"
+      },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
