4bec8164f379b36f65494c7343fbe2b36e2a4dd4
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-03-16 22:22:55 -0600

    create superadmin dashboard page

---
+++ src/app/superadmin/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: "BusMe - Administración superior",
+    description: "Dashboard",
+};
+export default function Page() {
+    return (
+      <div className="min-w-full h-screen bg-white">
+        <h1>Dashboard</h1>
+      </div>
+    );
+}
