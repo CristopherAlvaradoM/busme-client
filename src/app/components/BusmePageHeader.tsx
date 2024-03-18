092ac69cf7bff5b4002469e3e1981fc500993af1
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-03-18 16:19:10 -0600

    modify BusmePageHeader.tsx to accept icon parameter

--- src/app/components/BusmePageHeader.tsx
+++ src/app/components/BusmePageHeader.tsx
@@ -1,10 +1,22 @@
 import Image from "next/image";
 import UTZMGLogo from "@/assets/img/utzmg-logo.png";
+import {IoArrowBack, IoPencilOutline} from "react-icons/io5";
+import React from "react";
 
-const BusmePageHeader = ({title, username, rol }: { title: string; username: string; rol: string }) => {
+interface BusmePageHeaderProps {
+    title: string;
+    rol: string;
+    username: string;
+    showBackIcon?: boolean;
+}
+
+const BusmePageHeader = React.FC<BusmeTableProps> = ({ title, rol, username, showBackIcon = false }) => {
     return (
         <div className="flex justify-between items-center">
-            <p className="title-text">{title}</p>
+            <div className="flex items-center">
+                {showBackIcon && <IoArrowBack className="w-[35px] h-[35px] mr-4 cursor-pointer"/>}
+                <p className="title-text">{title}</p>
+            </div>
             <div className="flex items-center">
                 <div className="text-right">
                     <p className="caption-text">{rol}</p>
