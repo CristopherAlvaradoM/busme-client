2b40e7739a5a2276cc792f3c487c80873987649f
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-03-18 17:49:29 -0600

    create busme input component

---
+++ src/app/components/BusmeInput.tsx
@@ -0,0 +1,11 @@
+const BusmeInput = () => {
+    return (
+        <div>
+            <p className="caption-text">Nombre(s)</p>
+            <input type="text" placeholder="Ingresa un guaje"
+                   className="w-full rounded-lg bg-muted-200 p-3 font-poppins mt-2"/>
+        </div>
+    )
+}
+
+export default BusmeInput;
