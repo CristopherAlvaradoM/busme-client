76c71b536841c92acb12d9183d62d3194bfef961
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-03-18 16:28:01 -0600

    add back property in BusmePageHeader.tsx

--- src/app/components/BusmePageHeader.tsx
+++ src/app/components/BusmePageHeader.tsx
@@ -1,20 +1,26 @@
 import Image from "next/image";
 import UTZMGLogo from "@/assets/img/utzmg-logo.png";
-import {IoArrowBack, IoPencilOutline} from "react-icons/io5";
+import {IoArrowBack} from "react-icons/io5";
 import React from "react";
+import Link from "next/link";
 
 interface BusmePageHeaderProps {
     title: string;
     rol: string;
     username: string;
     showBackIcon?: boolean;
+    to?: string;
 }
 
-const BusmePageHeader = React.FC<BusmeTableProps> = ({ title, rol, username, showBackIcon = false }) => {
+const BusmePageHeader = React.FC<BusmeTableProps> = ({ title, rol, username, showBackIcon = false, to}) => {
     return (
         <div className="flex justify-between items-center">
             <div className="flex items-center">
-                {showBackIcon && <IoArrowBack className="w-[35px] h-[35px] mr-4 cursor-pointer"/>}
+                {showBackIcon &&
+                    <Link href={to}>
+                        <IoArrowBack className="w-[35px] h-[35px] mr-4 cursor-pointer"/>
+                    </Link>
+                }
                 <p className="title-text">{title}</p>
             </div>
             <div className="flex items-center">
