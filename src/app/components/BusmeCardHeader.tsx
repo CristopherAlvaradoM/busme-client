8eb490e37d414e0e41a83efa95fbdb9d467ce759
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-03-17 19:44:48 -0600

    add Card header component

---
+++ src/app/components/BusmeCardHeader.tsx
@@ -0,0 +1,10 @@
+const BusmeCardHeader = ({subtitle, linkText} : {subtitle: string; linkText: string}) => {
+    return (
+        <div className="p-5 flex justify-between items-center">
+            <h1 className="subtitle-text">{subtitle}</h1>
+            <h1 className="link-text">{linkText}</h1>
+        </div>
+    );
+}
+
+export default BusmeCardHeader;
