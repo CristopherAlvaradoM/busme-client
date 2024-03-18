49dd1f793cd42071e22d03b981c5f76c595caa5c
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-03-18 15:54:58 -0600

    fix icons to edit and delete in BusmeTable.tsx

--- src/app/components/BusmeTable.tsx
+++ src/app/components/BusmeTable.tsx
@@ -32,8 +32,8 @@ const BusmeTable: React.FC<BusmeTableProps> = ({ headers, data, showEditColumn =
                                 {cell}
                             </td>
                         ))}
-                        {showEditColumn && <td className="px-5 py-7">{row[0] === 'true' ? <IoPencilOutline color="#000"/> : null}</td>}
-                        {showDeleteColumn && <td className="px-5 py-7">{row[1] === 'true' ? <IoTrashOutline color="#000"/> : null}</td>}
+                        {showEditColumn && <td className="px-5 py-7 text-center"><IoPencilOutline className="w-[20px] h-[20px] cursor-pointer"/></td>}
+                        {showDeleteColumn && <td className="px-5 py-7 text-center"><IoTrashOutline className="w-[20px] h-[20px] text-danger cursor-pointer"/></td>}
                     </tr>
                 ))}
                 </tbody>
