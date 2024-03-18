85a4df9070ca6e6768ac54d76fb54f6040bf2103
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-03-17 20:09:53 -0600

    add data lists

--- src/app/superadmin/page.tsx
+++ src/app/superadmin/page.tsx
@@ -25,25 +25,31 @@ function textoSaludo(): string {
 
 const saludo = textoSaludo();
 
-const headers = ['Nombre', 'Correo electrónico', 'Teléfono', 'Rol', 'Fecha ingreso'];
-const data = [
+const workTeamHeaders = ['Nombre', 'Correo electrónico', 'Teléfono', 'Rol', 'Fecha ingreso'];
+const workTeamData = [
     ['Cristopher Yahir Alvarado Mombela', 'cristopher.alvarado.21s@utzmg.edu.mx', '3320217780', 'Superadministrador', '17/03/2024'],
     ['Braulio Israel Fernández Márquez', 'braulio.fernandez.21s@utzmg.edu.mx', '3311966694', 'Calidad', '17/03/2024'],
     ['Angélica Araceli Silva Palmas', 'angelica.silva.21s@utzmg.edu.mx', '3319698761', 'Administrador', '17/03/2024']
 ];
 
+const rolsHeaders = ['Nombre', 'Acceso a'];
+const rolsData = [
+    ['Superadministrador', 'Equipo de trabajo - Rols'],
+    ['Administrador', 'Monitoreo en tiempo real - Avisos - Transporte - Rutas - Estadísticas'],
+    ['Calidad', 'Buzón de quejas']
+];
 export default function Page() {
     return (
         <div className="m-5 w-full h-full">
             <BusmeProfileHeader title={saludo + " " + "Anthony"} username={"Anthony"} rol={"Superadministrador"}/>
             <BusmeCard>
                 <BusmeCardHeader subtitle={"Lista de equipo de trabajo"} linkText={"Ver más"}/>
-                <BusmeTable headers={headers} data={data}/>
+                <BusmeTable headers={workTeamHeaders} data={workTeamData}/>
             </BusmeCard>
 
             <BusmeCard>
-                <BusmeCardHeader subtitle={"Lista de equipo de trabajo"} linkText={"Ver más"}/>
-                <BusmeTable headers={headers} data={data}/>
+                <BusmeCardHeader subtitle={"Lista de roles de administración"} linkText={"Ver más"}/>
+                <BusmeTable headers={rolsHeaders} data={rolsData}/>
             </BusmeCard>
         </div>
     );
