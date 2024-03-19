15aaf41f3a208b53b860a29e91b9da1b352a31e6
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-03-18 18:00:07 -0600

    adjust BusmeInput.tsx

--- src/app/components/BusmeInput.tsx
+++ src/app/components/BusmeInput.tsx
@@ -1,8 +1,16 @@
-const BusmeInput = () => {
+import React from "react";
+
+interface BusmeInputProps {
+ title: string;
+ placeholder: string;
+ type: string;
+}
+
+const BusmeInput : React.FC<BusmeInputProps> = ({title, placeholder, type}) => {
     return (
-        <div>
-            <p className="caption-text">Nombre(s)</p>
-            <input type="text" placeholder="Ingresa un guaje"
+        <div className="mt-5">
+            <p className="caption-text">{title}</p>
+            <input type={type} placeholder={placeholder}
                    className="w-full rounded-lg bg-muted-200 p-3 font-poppins mt-2"/>
         </div>
     )
