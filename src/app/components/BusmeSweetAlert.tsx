9404f267525b2644e1821c7942fd8057f5d4486b
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-03-27 14:58:50 -0600

    refactor BusmeSweetAlert code

--- src/app/components/BusmeSweetAlert.tsx
+++ src/app/components/BusmeSweetAlert.tsx
@@ -1,6 +1,14 @@
 import Swal from 'sweetalert2';
+export enum BusmeSweetAlertIconType {
+    Success = 'success',
+    Error = 'error',
+    Warning = 'warning',
+    Info = 'info',
+    Question = 'question'
+}
 
-export const BusmeSweetAlert = (title: string, text: string, icon: undefined) => {
+// Definir la función showSweetAlert con el enum como el tipo del tercer parámetro
+export const BusmeSweetAlert = (title: string, text: string, icon: BusmeSweetAlertIconType) => {
     Swal.fire({
         title: title,
         text: text,
