19afc56cb86230721ed45a62e27b1370d19d7af7
Angelica <angelica.silva.21s@utzmg.edu.mx>
2024-03-26 21:05:44 -0600

    Fixed BusmeButtonLogin component

--- src/app/components/BusmeButtonLogin.tsx
+++ src/app/components/BusmeButtonLogin.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 interface CustomButtonProps {
   text: string;
+  type?: "submit" | "reset" | "button"; 
   onClick?: () => void;
 }
 
-const CustomButton: React.FC<CustomButtonProps> = ({ text, onClick }) => {
+const CustomButton: React.FC<CustomButtonProps> = ({ text, type, onClick }) => {
   return (
     <button
+      type={type} 
       className={`bg-primary-800 hover:bg-primary-700 text-white text-lg font-semi-bold py-2 px-4 rounded-full w-full h-12`}
-      onClick={onClick} // Pasamos la función onClick directamente aquí
+      onClick={onClick}
     >
       {text}
     </button>
--- src/app/forget-password/page.tsx
+++ src/app/forget-password/page.tsx
@@ -35,6 +35,7 @@ export default function ForgetPasswordPage() {
         </div>
         <div className=''>
           <CustomButton
+            type='submit'
             text="Enviar" 
             //onClick={}
           />
