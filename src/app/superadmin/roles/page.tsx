0b0d39729852b009cb42a78d82d4357474387d84
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-03-27 12:06:11 -0600

    created superadmin/roles page

--- src/app/superadmin/roles/page.tsx
+++ src/app/superadmin/roles/page.tsx
@@ -1,13 +1,29 @@
+import BusmePageHeader from "@/app/components/BusmePageHeader";
+import BusmeCard from "@/app/components/BusmeCard";
+import BusmeCardButtonHeader from "@/app/components/BusmeCardButtonHeader";
+import {IoPersonAdd} from "react-icons/io5";
+import BusmeTable from "@/app/components/BusmeTable";
+
 export const metadata = {
     title: "BusMe - Superadmin",
     description: "Pagina de roles",
 };
+
+const headers = ['Nombre', 'Con acceso a'];
+const data = [
+    ['Superadministrador', 'Equipo de trabajo - Roles'],
+    ['Administrador', 'Transporte en tiempo real - Avisos - Transporte - Rutas - Estadísticas'],
+    ['Calidad', 'Buzón de quejas y sugerencias'],]
+
 export default function Page() {
     return (
-        <div className="flex h-screen w-full bg-muted-200">
-            <div className="flex feelx-col w-full h-full p-4 font-poppins">
-                <h1 className="text-black text-base font-semi-bold">Roles Page</h1>
-            </div>
+        <div>
+            <BusmePageHeader title={"Roles de administración"} rol={"Superadministrador"} username={"Anthony"}/>
+            <BusmeCard>
+                <BusmeCardButtonHeader subtitle={"Lista de roles"} to={"/superadmin/roles/new-role"}
+                                       buttonText={"Agregar rol"} icon={IoPersonAdd}/>
+                <BusmeTable headers={headers} data={data} showEditColumn={true} showDeleteColumn={true}/>
+            </BusmeCard>
         </div>
     );
 }
