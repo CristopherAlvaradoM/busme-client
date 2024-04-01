fdbc25711674d032535dfd0d3b4ed1a5bb6b9f2f
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-03-31 19:13:16 -0600

    Fix BusmeModal component

--- src/app/components/BusmeModal.tsx
+++ src/app/components/BusmeModal.tsx
@@ -1,12 +1,16 @@
 import React, { FC, ReactNode, useEffect, useState } from 'react';
+import { IoClose } from "react-icons/io5";
+import { IconBaseProps } from "react-icons";
 
 interface ModalProps {
     isOpen: boolean;
     onClose: () => void;
+    showIcon?: boolean;
+    icon?: React.ComponentType<IconBaseProps>;
     children: ReactNode;
 }
 
-const BusmeModal: FC<ModalProps> = ({ isOpen, onClose, children }) => {
+const BusmeModal: FC<ModalProps> = ({ isOpen, onClose, showIcon = false, icon: Icon, children }) => {
     const [modalOpen, setModalOpen] = useState(false);
 
     useEffect(() => {
@@ -27,15 +31,17 @@ const BusmeModal: FC<ModalProps> = ({ isOpen, onClose, children }) => {
         <div className={`fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto ${modalOpen ? '' : 'hidden'}`} onClick={handleBackgroundClick}>
             <div className={`fixed inset-0 bg-black opacity-50 transition-opacity ${isOpen ? 'ease-out duration-300' : 'ease-in duration-200'}`}></div>
             {modalOpen && (
-                <div className={`relative w-auto max-w-md p-6 my-8 mx-auto bg-white rounded-lg shadow-lg transition-opacity ${isOpen ? 'ease-out duration-300' : 'ease-in duration-200'}`}>
-                    <button className="absolute top-0 right-0 m-3" onClick={onClose}>
-                        <svg className="w-6 h-6 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                  d="M6 18L18 6M6 6l12 12"></path>
-                        </svg>
-                    </button>
-                    <div className="mt-4">
+                <div
+                    className={`relative w-auto max-w-md p-6 my-8 mx-auto bg-white rounded-lg shadow-lg transition-opacity ${isOpen ? 'ease-out duration-300' : 'ease-in duration-200'}`}>
+                    <div className="flex justify-between">
+                        {showIcon && Icon && <Icon className="size-6 text-primary-600"/>}
+                        <div className="flex flex-1 justify-end">
+                            <IoClose
+                                className="size-6 hover:cursor-pointer hover:text-danger transition ease-in-out duration-300"
+                                onClick={onClose}/>
+                        </div>
+                    </div>
+                    <div className="mt-5">
                         {children}
                     </div>
                 </div>
