21d790b37bbfab808ae409280dd79089793c1ca4
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-03-31 21:13:04 -0600

    modify BusmeOption to envolve in select div

--- src/app/components/BusmeOption.tsx
+++ src/app/components/BusmeOption.tsx
@@ -1,25 +1,36 @@
-import React, { ReactNode } from 'react';
-import {IconBaseProps} from "react-icons";
+import React from 'react';
+import { IconBaseProps } from "react-icons";
+import { IoCheckmarkCircle } from "react-icons/io5";
 
 interface BusmeOptionProps {
     title: string;
     description: string;
     icon: React.ComponentType<IconBaseProps>;
     iconColor: string;
+    value: string;
+    selected: boolean;
+    onSelect: () => void;
 }
 
-const BusmeOption: React.FC<BusmeOptionProps> = ({ title, description, icon:Icon, iconColor }) => {
+const BusmeOption: React.FC<BusmeOptionProps> = ({ title, description, icon: Icon, iconColor, value, selected, onSelect }) => {
+    const handleCardClick = () => {
+        onSelect();
+    };
+
     return (
-        <div className="mt-5 py-3 px-4 border border-muted-500 rounded-lg">
+        <div
+            className={`mt-5 py-3 px-4 border border-muted-500 rounded-lg cursor-pointer ${selected ? 'bg-select-50 border-select-700' : 'bg-white'}`}
+            onClick={handleCardClick}
+        >
             <div className="flex items-center">
-                <Icon className={`size-6 ${iconColor}`}/>
+                <Icon className={`size-6 ${selected ? iconColor : ''}`} />
                 <div className="mx-4">
                     <p className="body-text">{title}</p>
                     <p className="modal-body-text">{description}</p>
                 </div>
-                <div className="flex flex-1 justify-end">
-                    <input type="radio"/>
-                </div>
+                {selected && (
+                    <IoCheckmarkCircle className="size-7 text-primary-600" />
+                )}
             </div>
         </div>
     );
