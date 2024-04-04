73ccfad1d790e74ff33822787a2c9920e163077b
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-04-04 15:56:47 -0600

    add conditional styles in BusmeMessage.tsx

--- src/app/components/BusmeMessage.tsx
+++ src/app/components/BusmeMessage.tsx
@@ -7,6 +7,7 @@ interface BusmeMessageProps {
     date: string;
     hour: string;
     isRead: boolean;
+    isSelected: boolean;
     onClick?: () => void;
 }
 
@@ -17,24 +18,30 @@ const BusmeMessage: React.FC<BusmeMessageProps> = ({
                                                        date,
                                                        hour,
                                                        isRead = false,
+                                                       isSelected = false,
                                                        onClick
                                                    }) => {
     return (
         <>
             <div
-                className={`p-5 w-full cursor-pointer transition ease-in-out duration-300 ${isRead ? 'hover:bg-primary-50' : 'bg-primary-50'}`}
+                className={`p-5 w-full cursor-pointer transition ease-in-out duration-300 ${isSelected ? 'bg-primary-500' : ''}`}
                 onClick={onClick}>
-                <p className="bold-body-text">{username}</p>
+                <div className="flex justify-between items-center py-2">
+                    <p className={`bold-body-text ${isSelected ? 'text-white' : ''}`}>{username}</p>
+                    {!isRead ? (
+                        <div className="size-2.5 bg-primary-500 rounded-full"/>
+                    ) : null}
+                </div>
                 <div className="flex items-center justify-between w-full">
                     <div>
-                        <p className="bold-body-text">{title}</p>
+                        <p className={`bold-body-text ${isSelected ? 'text-white' : ''}`}>{title}</p>
                         <div className="max-w-md">
-                            <p className="body-text overflow-hidden whitespace-nowrap overflow-ellipsis">{description}</p>
+                            <p className={`body-text overflow-hidden whitespace-nowrap overflow-ellipsis ${isSelected ? 'text-white' : ''}`}>{description}</p>
                         </div>
                     </div>
                     <div>
-                        <p className="body-text">{date}</p>
-                        <p className="body-text text-right">{hour}</p>
+                        <p className={`body-text ${isSelected ? 'text-white' : ''}`}>{date}</p>
+                        <p className={`body-text text-right ${isSelected ? 'text-white' : ''}`}>{hour}</p>
                     </div>
                 </div>
             </div>
