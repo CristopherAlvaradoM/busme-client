056e2f983d1b5982fea700162583d0fe552ee7a4
Angelica <angelica.silva.21s@utzmg.edu.mx>
2024-04-04 21:01:36 -0600

    Added structure for the new-route page

--- src/app/admin/routes/new-route/page.tsx
+++ src/app/admin/routes/new-route/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import BusmeCard from "@/app/components/BusmeCard";
 import BusmePageHeader from "@/app/components/BusmePageHeader";
 import React from "react";
 
@@ -8,10 +9,47 @@ export default function NewRoutePage() {
     <div>
       <BusmePageHeader 
         rol={"Administrador"} 
-        title={"Nuevo Ruta"} 
+        title={"Nueva Ruta"} 
         username={"GalloDeOro"} 
         showBackIcon={true} 
       />
+      <div>
+        <BusmeCard>
+          <div className="flex flex-col gap-5">
+            <div className="w-full flex flex-row gap-5">
+              <div className="flex flex-col w-4/12 gap-y-5">
+                <p className="subtitle-text">Información de la ruta</p>
+                <div className="flex flex-col gap-y-5">
+                  <span>Inpust</span>
+                  <span>Inpust</span>
+                </div>
+              </div>
+              <div className="flex flex-col w-8/12 gap-y-5">
+                <p className="subtitle-text">Chofer asignado</p>
+                <div className="grid grid-cols-2 gap-y-5">
+                  <span>Inpust</span>
+                  <span>Inpust</span>
+                  <span>Inpust</span>
+                  <span>Inpust</span>
+                </div>
+              </div>
+            </div>
+            <div className="w-full flex flex-row gap-5">
+              <div className="flex flex-col w-4/12 gap-y-5">
+                <p className="subtitle-text">Mapa de la ruta</p>
+                <div className="flex flex-col gap-y-5">
+                  <span>Inpust</span>
+                  <span>Inpust</span>
+                </div>
+              </div>
+              <div className="w-8/12 h-full">
+                <div> contededor del mapa</div>
+
+              </div>
+            </div>
+          </div>
+        </BusmeCard>
+      </div>
     </div>
   );
 }
