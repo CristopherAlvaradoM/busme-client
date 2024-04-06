41297faf51a2fb3f794cd750ceb4326c277f9e06
Angelica <angelica.silva.21s@utzmg.edu.mx>
2024-04-06 16:26:54 -0600

    Modified BusmeSecondaryButton component

--- src/app/components/BusmeSecondaryButton.tsx
+++ src/app/components/BusmeSecondaryButton.tsx
@@ -3,12 +3,17 @@ import React from "react";
 interface BusmeSecondaryButtonProps {
     title: string;
     disabled: boolean;
+    onClick?: () => void; // Hacer onClick opcional agregando "?" antes del ":"
 }
 
-const BusmeSecondaryButton: React.FC<BusmeSecondaryButtonProps> = ({title, disabled}) => {
+const BusmeSecondaryButton: React.FC<BusmeSecondaryButtonProps> = ({title, disabled, onClick}) => {
     return (
-        <button type="submit" disabled={disabled}
-                className="button-secondary button-secondary-hover w-full mt-6">
+        <button
+            type="submit" // Cambiar el tipo a "button" para evitar enviar formularios
+            disabled={disabled}
+            onClick={onClick} // Usar la función onClick si está definida
+            className="button-secondary button-secondary-hover w-full mt-6"
+        >
             {title}
         </button>
     )
