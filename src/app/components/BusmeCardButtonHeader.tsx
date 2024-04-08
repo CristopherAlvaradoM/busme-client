17f4e34da08345dbcef7b1a455538f683412f08a
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-04-08 09:20:31 -0600

    responsive design to BusmeCardButtonHeader.tsx

--- src/app/components/BusmeCardButtonHeader.tsx
+++ src/app/components/BusmeCardButtonHeader.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import {IconBaseProps} from "react-icons"; // Importa IconBaseProps para definir el tipo del ícono
 
 interface BusmeCardButtonHeaderProps {
-    subtitle: string;
+    subtitle?: string;
     to: string;
     buttonText: string;
     icon: React.ComponentType<IconBaseProps>; // Define el tipo del ícono
@@ -11,10 +11,14 @@ interface BusmeCardButtonHeaderProps {
 const BusmeCardButtonHeader: React.FC<BusmeCardButtonHeaderProps> = ({subtitle, to, buttonText, icon: Icon}) => {
     return (
         <div className="flex justify-between items-center">
+            {/* Título */}
             <h1 className="subtitle-text">{subtitle}</h1>
+
+            {/* Botón */}
             <Link href={to}>
-                <div className="flex items-center justify-center button-secondary button-secondary-hover">
-                    <Icon className="mr-3 w-[20px] h-[20px]"/>
+                <div
+                    className="button-secondary button-secondary-hover flex items-center justify-center">
+                    <Icon className="mr-2 w-5 h-5"/>
                     <p>{buttonText}</p>
                 </div>
             </Link>
