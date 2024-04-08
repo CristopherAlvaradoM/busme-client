9a0e84806a8779b553f38cd9f5e7bb544260e948
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-04-08 17:27:43 -0600

    responsive design to BusmeInput.tsx

--- src/app/components/BusmeInput.tsx
+++ src/app/components/BusmeInput.tsx
@@ -22,7 +22,7 @@ const BusmeInput: React.FC<BusmeInputProps> = ({name, title, placeholder, type, 
             value={value}
             className={`w-full rounded-lg bg-muted-200 p-3 font-poppins mt-2 border ${
                 validation ? 'border-danger' : 'border-muted-200'
-            } h-32 resize-none text-left`}
+            } h-32 resize-none text-left lg:text-base md:text-sm text-xs`}
         />
     ) : (
         <input
@@ -34,7 +34,7 @@ const BusmeInput: React.FC<BusmeInputProps> = ({name, title, placeholder, type, 
             value={value}
             className={`w-full rounded-lg bg-muted-200 p-3 font-poppins mt-2 border ${
                 validation ? 'border-danger' : 'border-muted-200'
-            }`}
+            } lg:text-base md:text-sm text-xs`}
         />
     );
     return (
