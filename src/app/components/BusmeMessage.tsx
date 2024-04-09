39e91b4de6beab0d940c87c3e5342b795852877d
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-04-08 23:34:28 -0600

    responsive design in BusmeMessage.tsx

--- src/app/components/BusmeMessage.tsx
+++ src/app/components/BusmeMessage.tsx
@@ -28,21 +28,20 @@ const BusmeMessage: React.FC<BusmeMessageProps> = ({
                 onClick={onClick}>
                 <div className="flex justify-between items-center py-2">
                     <p className={`bold-body-text ${isSelected ? 'text-white' : ''}`}>{username}</p>
-                    {!isRead ? (
-                        <div className="size-2.5 bg-primary-500 rounded-full"/>
-                    ) : null}
+                    {!isRead && <div className="size-2.5 bg-primary-500 rounded-full"/>}
                 </div>
-                <div className="flex items-center justify-between w-full">
-                    <div>
+                <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between w-full">
+                    <div
+                        className="mb-3 sm:mb-0 max-w-full"> {/* Cambio max-w-md a max-w-full en dispositivos móviles */}
                         <p className={`bold-body-text ${isSelected ? 'text-white' : ''}`}>{title}</p>
                         <div className="max-w-md">
                             <p className={`body-text overflow-hidden whitespace-nowrap overflow-ellipsis ${isSelected ? 'text-white' : ''}`}>{description}</p>
                         </div>
                     </div>
-                    <div>
-                        <p className={`body-text ${isSelected ? 'text-white' : ''}`}>{date}</p>
-                        <p className={`body-text text-right ${isSelected ? 'text-white' : ''}`}>{hour}</p>
-                    </div>
+                </div>
+                <div className="flex justify-between w-full sm:w-auto mt-2">
+                    <p className={`body-text ${isSelected ? 'text-white' : ''}`}>{date}</p>
+                    <p className={`body-text text-right ${isSelected ? 'text-white' : ''}`}>{hour}</p>
                 </div>
             </div>
             <div className="border-[1px] border-muted-300 w-full"/>
