c58e26bacfe666f4099fd0285ec76d7017516d40
Angelica <angelica.silva.21s@utzmg.edu.mx>
2024-04-15 04:46:39 -0600

    Modified Admin Layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -23,9 +23,9 @@ export default function AdminLayout({
   };
   
   return (
-    <div className="font-poppins bg-complementary-100 w-full min-h-screen">
+    <div className="font-poppins bg-complementary-100 w-full min-h-screen overflow-auto">
       {/* Botón para abrir el sidebar en pantallas pequeñas */}
-      <div className="flex w-full justify-end px-5 py-5 bg-complementary-100 md:hidden">
+      <div className="flex w-full justify-end px-5 py-5 bg-complementary-100 sm:hidden">
         <button onClick={toggleSidebar}>
           <AlignJustify className="text-3xl text-black" />
         </button>
@@ -39,12 +39,12 @@ export default function AdminLayout({
       {/* Capa de color gris cuando el sidebar está abierto */}
       {isSidebarOpen && (
         <div
-          className="fixed top-0 left-0 z-40 w-full h-full bg-muted-950 bg-opacity-60"
+          className="fixed top-0 left-0 z-40 w-full h-full bg-muted-950 bg-opacity-60 sm:bg-transparent"
           onClick={toggleSidebar} // Cerrar el sidebar al hacer clic en la capa de color gris
         />
       )}
       {/* Contenido */}
-      <div className={`ml-0 md:ml-64 px-5 md:py-5 max-w-full h-full ${isSidebarOpen && 'overflow-hidden'}`}>
+      <div className={`ml-0 sm:ml-64 px-5 sm:py-5 max-w-full h-full ${isSidebarOpen && 'overflow-hidden'}`}>
         {children}
       </div>
     </div>
